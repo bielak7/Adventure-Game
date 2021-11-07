0770523be551a3be1e77f48a3131fab1e2ea68bd
bielak7 <92972176+bielak7@users.noreply.github.com>
2021-11-07 17:05:03 +0100

    Adding new game to Github - not working correct

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ print("Welcome", name, "to this adventure!")
 answer = input("you are in the dirt road it has come to an end  and you can go left or right? ").lower()
 
 if answer == "left":
-    elif answer == "right":
+    print("You go along the forest")
 
-    else:
+elif answer == "right":
+    print("You go along the forest")
+
+else:
     print("Not a valid option. You loose.")
