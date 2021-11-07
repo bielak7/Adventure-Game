7894649afd555304b5bc063b62f7f8da903eab3b
bielak7 <92972176+bielak7@users.noreply.github.com>
2021-11-07 16:57:47 +0100

    Adding new game to Github - not working correct

---
+++ main.py
@@ -0,0 +1,10 @@
+name = input("Type your name")
+print("Welcome", name, "to this adventure!")
+
+answer = input("you are in the dirt road it has come to an end  and you can go left or right? ").lower()
+
+if answer == "left":
+    elif answer == "right":
+
+    else:
+    print("Not a valid option. You loose.")
